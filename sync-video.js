8ad7583339689d675bdbfc32ff0e25e7df436332
Ahmed Al-Sudani <alsudani.ahmed@gmail.com>
2016-03-07 21:57:47 -0500

    Suiting up

--- sync-video.js
+++ sync-video.js
@@ -9,99 +9,110 @@
  */
 
 /*global $, Firebase, FIREBASE_ROOT */
+(function() {
+    function syncVideo(video, id) {
+        'use strict';
+        var $video = $(video),
+            fb = null,
+            playing = null,
+            seekTime = null,
+            playTime = null,
+            lockRemoteState = false,
+            initialized = false,
+            initialTimePulled = false,
+            lastRecordedTime = 0,
 
-function syncVideo(video, id) {
-    'use strict';
-    var $video = $(video),
-        fb = null,
-        playing = null,
-        seekTime = null,
-        playTime = null,
-        lockRemoteState = false,
-        initialized = false,
-        initialTimePulled = false,
-        lastRecordedTime = 0,
+            // Push playing/paused to Firebase/playing
+            pushPlayState = function (e) {
+                if (lockRemoteState === false) {
+                    playing.set(!e.target.paused);
+                }
+            },
 
-        // Push playing/paused to Firebase/playing
-        pushPlayState = function (e) {
-            if (lockRemoteState === false) {
-                playing.set(!e.target.paused);
-            }
-        },
+            // Push current video time to Firebase/play-time
+            pushPlayTime = function (e) {
+                if (lockRemoteState === false && initialTimePulled === true) {
+                    playTime.set(e.target.currentTime);
+                }
+            },
 
-        // Push current video time to Firebase/play-time
-        pushPlayTime = function (e) {
-            if (lockRemoteState === false && initialTimePulled === true) {
-                playTime.set(e.target.currentTime);
-            }
-        },
+            // Push current video time to Firebase/seek-time
+            pushSeekTime = function (e) {
+                if (initialTimePulled === true && lockRemoteState === false) {
+                    seekTime.set(e.target.currentTime);
+                }
+                /*
+                 * I probably deserve to be killed for this but here's the
+                 * reasoning. The browser API does not seem to distinguish between
+                 * the user changing the time and a script changing it so we know
+                 * we have pulled initial time when this script is called
+                 */
+                initialTimePulled = true;
+            },
 
-        // Push current video time to Firebase/seek-time
-        pushSeekTime = function (e) {
-            if (initialTimePulled === true && lockRemoteState === false) {
-                seekTime.set(e.target.currentTime);
-            }
-            /*
-             * I probably deserve to be killed for this but here's the
-             * reasoning. The browser API does not seem to distinguish between
-             * the user changing the time and a script changing it so we know
-             * we have pulled initial time when this script is called
-             */
-            initialTimePulled = true;
-        },
+            // If playingState contains true, play. Otherwise pause.
+            updateLocalPlayState = function (playingState) {
+                lockRemoteState = true;
+                //noinspection JSLint
+                playingState.val() === true ? video.play() : video.pause();
+                lockRemoteState = false;
+            },
 
-        // If playingState contains true, play. Otherwise pause.
-        updateLocalPlayState = function (playingState) {
-            lockRemoteState = true;
-            //noinspection JSLint
-            playingState.val() === true ? video.play() : video.pause();
-            lockRemoteState = false;
-        },
+            updateLocalTime = function (receivedTime, options) {
+                // Silly JS
+                options = options || {};
+                lockRemoteState = true;
 
-        updateLocalTime = function (receivedTime, options) {
-            // Silly JS
-            options = options || {};
-            lockRemoteState = true;
+                /*
+                 * Check for initialized so we don't go to the last seek unless
+                 * it's actually a change
+                 *
+                 * Check for options.force so we can go to the last play position
+                 * as part of the initialization (while initialized === false)
+                 */
+                if (initialized === true || options.force === true) {
+                    video.currentTime = receivedTime.val();
+                }
+                lockRemoteState = false;
+            },
 
-            /*
-             * Check for initialized so we don't go to the last seek unless
-             * it's actually a change
-             *
-             * Check for options.force so we can go to the last play position
-             * as part of the initialization (while initialized === false)
-             */
-            if (initialized === true || options.force === true) {
-                video.currentTime = receivedTime.val();
-            }
-            lockRemoteState = false;
-        },
+            initialize = function () {
+                fb = new Firebase(FIREBASE_ROOT)
+                    .child(encodeURIComponent(id)
+                           .replace(/\./g, '%2E'));
+                playing = fb.child('playing');
+                seekTime = fb.child('seek-time');
+                playTime = fb.child('play-time');
 
-        initialize = function () {
-            fb = new Firebase(FIREBASE_ROOT)
-                .child(encodeURIComponent(id)
-                    .replace(/\./g, '%2E'));
-            playing = fb.child('playing');
-            seekTime = fb.child('seek-time');
-            playTime = fb.child('play-time');
+                playing.on('value', updateLocalPlayState);
+                seekTime.on('value', updateLocalTime);
+                playTime.once('value', function (e) {
+                    updateLocalTime(e, { force: true });
+                });
 
-            playing.on('value', updateLocalPlayState);
-            seekTime.on('value', updateLocalTime);
-            playTime.once('value', function (e) {
-                updateLocalTime(e, { force: true });
-            });
+                $video.on('play', pushPlayState);
+                $video.on('pause', pushPlayState);
+                $video.on('seeked', pushSeekTime);
+                $video.on('timeupdate', pushPlayTime);
 
-            $video.on('play', pushPlayState);
-            $video.on('pause', pushPlayState);
-            $video.on('seeked', pushSeekTime);
-            $video.on('timeupdate', pushPlayTime);
+                initialized = true;
+            };
 
-            initialized = true;
-        };
+        if (video.readyState >= video.HAVE_METADATA) {
+            initialize();
+        } else {
+            $video.on('loadedmetadata', initialize);
+        }
 
-    if (video.readyState >= video.HAVE_METADATA) {
-        initialize();
-    } else {
-        $video.on('loadedmetadata', initialize);
     }
 
-}
+    if( typeof exports !== 'undefined' ) {
+        if( typeof module !== 'undefined' && module.exports ) {
+            exports = module.exports = syncVideo;
+        }
+        exports.syncVideo = syncVideo;
+    }
+    else {
+        window.syncVideo = syncVideo;
+    }
+})();
